38e9bce8e6b64cbafb0512df960ff0336dcea7b5
Atomu-Tetuwan <atomu.10.18@icloud.com>
2024-04-08 17:18:43 +0900

    sum test

---
+++ sum/sum.go
@@ -0,0 +1,38 @@
+package sum
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numberToSum ...[]int) []int {
+	// lengthOfNumbers := len(numberToSum)
+	// sums = make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numberToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	var sums []int
+	for _, number := range numberToSum {
+		sums = append(sums, Sum(number))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+				tail := numbers[1:]
+				sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
